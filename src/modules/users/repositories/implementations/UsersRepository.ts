2cb4911e33ab8eb1820c9fd474f904f628328020
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-12 23:10:57 -0300

    Initial commit.

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOneOrFail(user_id, {
+      relations: ["games"]
+    });
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("select * from users order by first_name");
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query("select * from users where users.first_name = $1 and users.last_name = $2", [first_name, last_name]);
+  }
+}
